ac78499ecb93e171cc202c8eaf1883b1532209ac
Kyle Wydock <kswydock@gmail.com>
2022-05-11 20:37:51 -0700

    practice_activity_7_part-1

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polling/', include('polling.urls')),
+    path('', include('blogging.urls')),
 
 ]
 
